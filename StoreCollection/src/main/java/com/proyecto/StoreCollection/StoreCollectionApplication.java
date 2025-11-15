9dbb87994875eac73ba09e29c98d27ed86f84501
JoseEM26 <joseangelespinozamorales@gmail.com>
2025-11-14 19:49:10 -0500

    first commit

---
+++ StoreCollection/src/main/java/com/proyecto/StoreCollection/StoreCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.StoreCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCollectionApplication.class, args);
+	}
+
+}
---
+++ StoreCollection/src/test/java/com/proyecto/StoreCollection/StoreCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.StoreCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
