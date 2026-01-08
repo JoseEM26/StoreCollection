1be671a2524eada63f9bf8e73b9aff17b798a85a
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-01-08 00:32:16 -0500

    production v2

--- StoreCollectionFrontEnd/environment.ts
+++ StoreCollectionFrontEnd/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //USAR ESTO SOLO PARA PRODUCCION
-  //apiUrl: 'https://zooming-quietude-production.up.railway.app'  
-  apiUrl: 'http://localhost:8080'  
+  apiUrl: 'https://zooming-quietude-production.up.railway.app'  
+  //apiUrl: 'http://localhost:8080'  
 };
 
