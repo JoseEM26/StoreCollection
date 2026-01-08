cb6aaaa7404e65a49967a9c3bd073ed343c6204d
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-01-08 01:02:30 -0500

    bloqueo de boton create store

--- StoreCollectionFrontEnd/environment.ts
+++ StoreCollectionFrontEnd/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  //USAR ESTO SOLO PARA PRODUCCION
   apiUrl: 'https://zooming-quietude-production.up.railway.app'  
   //apiUrl: 'http://localhost:8080'  
 };
