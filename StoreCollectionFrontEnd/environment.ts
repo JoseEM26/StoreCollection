c8de94d1454fe538deabd4789c20f6526577edda
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-01-08 00:29:32 -0500

    este layaout esta mejor

--- StoreCollectionFrontEnd/environment.ts
+++ StoreCollectionFrontEnd/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  //USAR ESTO SOLO PARA PRODUCCION
   //apiUrl: 'https://zooming-quietude-production.up.railway.app'  
   apiUrl: 'http://localhost:8080'  
 };
