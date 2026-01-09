8660cf828e31be076a90b33da7b00dd05ff01125
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-01-09 13:08:23 -0500

    env correcto

--- StoreCollectionFrontEnd/environment.ts
+++ StoreCollectionFrontEnd/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-   //apiUrl: 'https://zooming-quietude-production.up.railway.app'  
- apiUrl: 'http://localhost:8080'  
+  apiUrl: 'https://zooming-quietude-production.up.railway.app'  
+  //apiUrl: 'http://localhost:8080'  
 };
 
